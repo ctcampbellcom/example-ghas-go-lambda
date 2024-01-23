b0c904b8a0651bb61c18bd679b268f11f03cdcc1
Chris Campbell <808531+ctcampbell@users.noreply.github.com>
2024-01-23 12:40:03 +0000

    Add SQL code

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/od-ctcampbell/go-lambda
 
 go 1.21.6
 
-require github.com/aws/aws-lambda-go v1.45.0
+require (
+	github.com/aws/aws-lambda-go v1.45.0
+	github.com/go-sql-driver/mysql v1.7.1
+)
--- lambda.go
+++ lambda.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"database/sql"
+	"flag"
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type MyEvent struct {
@@ -15,10 +20,36 @@ type MyResponse struct {
 	Message string `json:"Answer"`
 }
 
+var pool *sql.DB // Database connection pool.
+
+func DbHandler(category string) {
+	q := fmt.Sprintf("SELECT ITEM,PRICE FROM PRODUCT WHERE ITEM_CATEGORY='%s' ORDER BY PRICE", category)
+	pool.Query(q)
+}
+
 func HandleLambdaEvent(event *MyEvent) (*MyResponse, error) {
 	if event == nil {
 		return nil, fmt.Errorf("received nil event")
 	}
+
+	dsn := flag.String("dsn", os.Getenv("DSN"), "connection data source name")
+	if len(*dsn) == 0 {
+		log.Fatal("missing dsn flag")
+	}
+
+	var err error
+	pool, err = sql.Open("mysql", *dsn)
+	if err != nil {
+		// This will not be a connection error, but a DSN parse error or
+		// another initialization error.
+		log.Fatal("unable to use data source name", err)
+	}
+	defer pool.Close()
+
+	pool.SetConnMaxLifetime(0)
+	pool.SetMaxIdleConns(3)
+	pool.SetMaxOpenConns(3)
+
 	return &MyResponse{Message: fmt.Sprintf("%s is %d years old!", event.Name, event.Age)}, nil
 }
 
